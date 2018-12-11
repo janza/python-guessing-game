9195a60031ecfef2223c64c5a25417eabfd57c46
Sandeep Thukral <sandeep.thukral@insided.com>
2018-12-11 09:50:36 +0100

    more refactoring and some comments

--- index.py
+++ index.py
@@ -1,26 +1,26 @@
 from random import randint
 
-MIN_ATTEMPTS = 0
+BEST_ROUND_NUMBER_OF_GUESSES = 0
 
 class Game:
   MIN = 0
   MAX = 100
-  count = 0
+  number_of_guesses = 0
 
   SYSTEM_GUESS = 0
 
   def start_game(self):
     print "starting game"
-    self.init()
-    self.SYSTEM_GUESS = randint(self.MIN, self.MAX)
-    print self.SYSTEM_GUESS
+    self.init_game()
     self.roll()
 
-  def init(self):
+  def init_game(self):
     self.MIN = 0
     self.MAX = 100
-    self.count = 0
+    self.number_of_guesses = 0
+    self.SYSTEM_GUESS = randint(self.MIN, self.MAX)
 
+  # this function asks for and processes the user's input
   def roll(self):
     guess = raw_input(self.get_statement())
     guess = self.parse_input(guess)
@@ -31,8 +31,9 @@ class Game:
 
   def process_correct_guess(self):
     print "You guessed it right!"
-    print 'It only took you {0} guesses'.format(self.count)
-    self.update_best_round(self.count)
+    print 'It only took you {0} guesses'.format(self.number_of_guesses)
+    # update the ccounter of the user's best round
+    self.update_best_round(self.number_of_guesses)
     self.ask_again()
 
   def process_incorrect_guess(self, guess):
@@ -47,6 +48,7 @@ class Game:
   def get_statement(self):
     return 'Enter a number between {0} and {1} - '.format(self.MIN, self.MAX)
 
+  # parse the user's input and return return only a valid input
   def parse_input(self, input):
     if (not input.isdigit()):
       print('Your guess should be a number.')
@@ -54,7 +56,8 @@ class Game:
     if (int(input) < self.MIN or int(input) > self.MAX):
       print 'Your guess should be between {0} and {1}'.format(self.MIN, self.MAX)
       self.roll()
-    self.count = self.count + 1
+    # since this was a valid input, we increment the number of guesses
+    self.number_of_guesses = self.number_of_guesses + 1
     return input
 
   def ask_again(self):
@@ -63,18 +66,19 @@ class Game:
       self.start_game()
     if input.lower() == 'n':
       self.print_best_attempt_and_exit()
+    # it seems the user has not answered y or n. Ask again
     self.ask_again()
 
   def update_best_round(self, attempts):
-    global MIN_ATTEMPTS
-    if MIN_ATTEMPTS == 0 or attempts < MIN_ATTEMPTS:
-      MIN_ATTEMPTS = attempts
+    global BEST_ROUND_NUMBER_OF_GUESSES
+    if BEST_ROUND_NUMBER_OF_GUESSES == 0 or attempts < BEST_ROUND_NUMBER_OF_GUESSES:
+      BEST_ROUND_NUMBER_OF_GUESSES = attempts
 
   def print_best_attempt_and_exit(self):
-    global MIN_ATTEMPTS
+    global BEST_ROUND_NUMBER_OF_GUESSES
     print ' '
     print 'Thanks for playing!'
-    print 'Your best round was {0} guesses.'.format(MIN_ATTEMPTS)
+    print 'Your best round was {0} guesses.'.format(BEST_ROUND_NUMBER_OF_GUESSES)
     exit()
 
 if __name__ == '__main__':
